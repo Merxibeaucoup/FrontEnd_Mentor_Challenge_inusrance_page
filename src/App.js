452aed9bffacc27cea3baba193c7653d67c53cc2
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-24 23:03:11 -0400

    2 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Different from "./components/Different";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Nav />
       <Header />
+      <Different />
     </div>
   );
 };
---
+++ src/components/Different.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Different = () => {
+  return (
+    <div className="diff">
+      <hr className="diff__hr" />
+      <h2 className="diff__title">We're different</h2>
+      <div className="diff__items">
+        <div className="diff__items__wrapper">
+          <div className="diff__items__icon">icons</div>
+          <h4 className="diff__items__heading">Snappy Process</h4>
+          <p className="diff__items__paragraph">
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </p>
+        </div>
+        <div className="diff__items__wrapper">
+          <div className="diff__items__icon">icons</div>
+          <h4 className="diff__items__heading">Snappy Process</h4>
+          <p className="diff__items__paragraph">
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </p>
+        </div>
+        <div className="diff__items__wrapper">
+          <div className="diff__items__icon">icons</div>
+          <h4 className="diff__items__heading">Snappy Process</h4>
+          <p className="diff__items__paragraph">
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </p>
+        </div>
+      </div>
+
+      <div className="diff__rectangle">
+        <div className="diff__ractangle__left">
+          <h2 className="diff__rectangle__heading">
+            Find out more about how we work
+          </h2>
+        </div>
+        <div className="diff__ractangle__right">
+          <button className="diff__rectangle__button">how we work</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Different;
